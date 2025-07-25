8213964ccbe85f32b5350f804c8e50b35067411a
gurrier <tmugan@gmail.com>
2025-07-25 16:08:44 +1000

    Update powerpal_ble.h
    Persistence of total and daily energy to survive restarts of ESP32 board

--- components/powerpal_ble/powerpal_ble.h
+++ components/powerpal_ble/powerpal_ble.h
@@ -1,4 +1,3 @@
-// powerpal_ble.h
 #pragma once
 
 #include "esphome/core/component.h"
@@ -6,7 +5,9 @@
 #include "esphome/components/esp32_ble_tracker/esp32_ble_tracker.h"
 #include "esphome/components/sensor/sensor.h"
 #include "esphome/core/defines.h"
-#include <nvs_flash.h>
+#include "esphome/core/helpers.h"
+
+#include <nvs_flash.h>                   // ESP‑IDF non-volatile storage
 #include <nvs.h>
 
 #ifdef USE_TIME
@@ -24,86 +25,134 @@ namespace powerpal_ble {
 
 namespace espbt = esphome::esp32_ble_tracker;
 
-// Measurement struct
 struct PowerpalMeasurement {
   uint16_t pulses;
   time_t timestamp;
   uint32_t watt_hours;
   float cost;
+  // bool is_peak;
 };
 
-// BLE UUIDs
+
+
 static const espbt::ESPBTUUID POWERPAL_SERVICE_UUID =
     espbt::ESPBTUUID::from_raw("59DAABCD-12F4-25A6-7D4F-55961DCE4205");
-// ... other characteristic UUIDs as needed ...
+static const espbt::ESPBTUUID POWERPAL_CHARACTERISTIC_PAIRING_CODE_UUID =
+    espbt::ESPBTUUID::from_raw("59DA0011-12F4-25A6-7D4F-55961DCE4205");  // indicate, notify, read, write
+static const espbt::ESPBTUUID POWERPAL_CHARACTERISTIC_READING_BATCH_SIZE_UUID =
+    espbt::ESPBTUUID::from_raw("59DA0013-12F4-25A6-7D4F-55961DCE4205");  // indicate, notify, read, write
+static const espbt::ESPBTUUID POWERPAL_CHARACTERISTIC_MEASUREMENT_UUID =
+    espbt::ESPBTUUID::from_raw("59DA0001-12F4-25A6-7D4F-55961DCE4205");  // notify, read, write
+static const espbt::ESPBTUUID POWERPAL_CHARACTERISTIC_UUID_UUID =
+    espbt::ESPBTUUID::from_raw("59DA0009-12F4-25A6-7D4F-55961DCE4205");  // indicate, notify, read, write
+static const espbt::ESPBTUUID POWERPAL_CHARACTERISTIC_SERIAL_UUID =
+    espbt::ESPBTUUID::from_raw("59DA0010-12F4-25A6-7D4F-55961DCE4205");  // indicate, notify, read, write
 
-static const uint8_t seconds_in_minute = 60;
-static const float kw_to_w_conversion = 1000.0;
+static const espbt::ESPBTUUID POWERPAL_BATTERY_SERVICE_UUID = espbt::ESPBTUUID::from_uint16(0x180F);
+static const espbt::ESPBTUUID POWERPAL_BATTERY_CHARACTERISTIC_UUID = espbt::ESPBTUUID::from_uint16(0x2A19);
+
+
+
+static const uint8_t seconds_in_minute = 60;    // seconds
+static const float kw_to_w_conversion = 1000.0;    // conversion ratio
+
+
 
 class Powerpal : public esphome::ble_client::BLEClientNode, public Component {
+  // class Powerpal : public esphome::ble_client::BLEClientNode, public PollingComponent {
  public:
   void setup() override;
-  void dump_config() override;
+  // void loop() override;
   void gattc_event_handler(esp_gattc_cb_event_t event, esp_gatt_if_t gattc_if,
-                           esp_ble_gattc_cb_param_t* param) override;
-  void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t* param) override;
-
-  // Generated setters
-  void set_ble_client(ble_client::BLEClient* client) { this->parent_ = client; }
-  void set_pairing_code(uint32_t pairing_code);
-  void set_notification_interval(uint8_t reading_batch_size);
-  void set_pulses_per_kwh(float pulses_per_kwh) { pulses_per_kwh_ = pulses_per_kwh; }
-  void set_energy_cost(double energy_cost) { energy_cost_ = energy_cost; }
+                           esp_ble_gattc_cb_param_t *param) override;
+  void gap_event_handler(esp_gap_ble_cb_event_t event, esp_ble_gap_cb_param_t *param) override;
+  void dump_config() override;
+  // float get_setup_priority() const override { return setup_priority::DATA; }
+  float get_setup_priority() const override { return setup_priority::AFTER_WIFI; }
+  void set_ble_client(ble_client::BLEClient *client) { this->parent_ = client; }
+  void set_battery(sensor::Sensor *battery) { battery_ = battery; }
+  void set_power_sensor(sensor::Sensor *power_sensor) { power_sensor_ = power_sensor; }
+  void set_energy_sensor(sensor::Sensor *energy_sensor) { energy_sensor_ = energy_sensor; }
+  void set_daily_energy_sensor(sensor::Sensor *daily_energy_sensor) { daily_energy_sensor_ = daily_energy_sensor; }
+  void set_cost_sensor(sensor::Sensor *cost_sensor) { cost_sensor_ = cost_sensor;}
+  void set_pulses_sensor(sensor::Sensor *pulses_sensor) { pulses_sensor_ = pulses_sensor;}
+  void set_watt_hours(sensor::Sensor *watt_hours_sensor) {watt_hours_sensor_ = watt_hours_sensor;}
+  void set_timestamp(sensor::Sensor *timestamp_sensor) { timestamp_sensor_ = timestamp_sensor;}
+  void set_daily_pulses_sensor(sensor::Sensor *daily_pulses_sensor) { daily_pulses_sensor_ = daily_pulses_sensor;}
 #ifdef USE_TIME
-  void set_time(time::RealTimeClock* time) { time_ = time; }
+  void set_time(time::RealTimeClock *time) { time_ = time; }
 #endif
-
-  // Sensor setters
-  void set_battery(sensor::Sensor* battery) { battery_ = battery; }
-  void set_power_sensor(sensor::Sensor* power_sensor) { power_sensor_ = power_sensor; }
-  void set_energy_sensor(sensor::Sensor* energy_sensor) { energy_sensor_ = energy_sensor; }
-  void set_daily_energy_sensor(sensor::Sensor* daily_energy_sensor) { daily_energy_sensor_ = daily_energy_sensor; }
-  void set_cost_sensor(sensor::Sensor* cost_sensor) { cost_sensor_ = cost_sensor; }
-  void set_pulses_sensor(sensor::Sensor* pulses_sensor) { pulses_sensor_ = pulses_sensor; }
-  void set_daily_pulses_sensor(sensor::Sensor* daily_pulses_sensor) { daily_pulses_sensor_ = daily_pulses_sensor; }
-  void set_watt_hours(sensor::Sensor* watt_hours_sensor) { watt_hours_sensor_ = watt_hours_sensor; }
-  void set_timestamp(sensor::Sensor* timestamp_sensor) { timestamp_sensor_ = timestamp_sensor; }
+  void set_pulses_per_kwh(float pulses_per_kwh) { pulses_per_kwh_ = pulses_per_kwh; }
+  void set_pairing_code(uint32_t pairing_code) {
+    pairing_code_[0] = (pairing_code & 0x000000FF);
+    pairing_code_[1] = (pairing_code & 0x0000FF00) >> 8;
+    pairing_code_[2] = (pairing_code & 0x00FF0000) >> 16;
+    pairing_code_[3] = (pairing_code & 0xFF000000) >> 24;
+  }
+  void set_notification_interval(uint8_t reading_batch_size) { reading_batch_size_[0] = reading_batch_size; }
+  void set_device_id(std::string powerpal_device_id) { powerpal_device_id_ = powerpal_device_id; }
+  void set_apikey(std::string powerpal_apikey) { powerpal_apikey_ = powerpal_apikey; }
+  void set_energy_cost(double energy_cost) { energy_cost_ = energy_cost; }
 
  protected:
+  // Persisted daily pulses:
+  nvs_handle_t nvs_handle_;
+  bool nvs_ok_{false};
+
+  uint64_t daily_pulses_{0};
+  uint64_t total_pulses_{0};  
+
+  std::string pkt_to_hex_(const uint8_t *data, uint16_t len);
+  void decode_(const uint8_t *data, uint16_t length);
+  void parse_battery_(const uint8_t *data, uint16_t length);
+  void parse_measurement_(const uint8_t *data, uint16_t length);
+ 
+  std::string uuid_to_device_id_(const uint8_t *data, uint16_t length);
+  std::string serial_to_apikey_(const uint8_t *data, uint16_t length);
+
+
+  bool authenticated_;
+
+  sensor::Sensor *battery_{nullptr};
+  sensor::Sensor *power_sensor_{nullptr};
+  sensor::Sensor *energy_sensor_{nullptr};
+  sensor::Sensor *daily_energy_sensor_{nullptr};
+  sensor::Sensor *cost_sensor_{nullptr};
+  sensor::Sensor *pulses_sensor_{nullptr};
+  sensor::Sensor *daily_pulses_sensor_{nullptr};
+  sensor::Sensor *watt_hours_sensor_{nullptr};
+  sensor::Sensor *timestamp_sensor_{nullptr};
+ 
+
 #ifdef USE_TIME
-  optional<time::RealTimeClock*> time_;
+  optional<time::RealTimeClock *> time_{};
 #endif
-  nvs_handle_t nvs_handle_;             // NVS handle for storage
-  uint64_t daily_pulses_{0};           // persisted value
-  float pulses_per_kwh_{0}, pulse_multiplier_{0};
-  double energy_cost_{0};
-  uint64_t total_pulses_{0};
   uint16_t day_of_last_measurement_{0};
 
   uint8_t pairing_code_[4];
   uint8_t reading_batch_size_[4] = {0x01, 0x00, 0x00, 0x00};
+  float pulses_per_kwh_;
+  float pulse_multiplier_;
+  
 
-  // Sensors
-  sensor::Sensor* battery_{nullptr};
-  sensor::Sensor* power_sensor_{nullptr};
-  sensor::Sensor* energy_sensor_{nullptr};
-  sensor::Sensor* daily_energy_sensor_{nullptr};
-  sensor::Sensor* cost_sensor_{nullptr};
-  sensor::Sensor* pulses_sensor_{nullptr};
-  sensor::Sensor* daily_pulses_sensor_{nullptr};
-  sensor::Sensor* watt_hours_sensor_{nullptr};
-  sensor::Sensor* timestamp_sensor_{nullptr};
+  uint8_t stored_measurements_count_{0};
+  std::vector<PowerpalMeasurement> stored_measurements_;
+  std::string powerpal_device_id_; 
+  std::string powerpal_apikey_; 
+  double energy_cost_;
 
-  // Helpers
-  std::string pkt_to_hex_(const uint8_t* data, uint16_t len);
-  void decode_(const uint8_t* data, uint16_t length);
-  void parse_battery_(const uint8_t* data, uint16_t length);
-  void parse_measurement_(const uint8_t* data, uint16_t length);
-  std::string uuid_to_device_id_(const uint8_t* data, uint16_t length);
-  std::string serial_to_apikey_(const uint8_t* data, uint16_t length);
+  uint16_t pairing_code_char_handle_ = 0x2e;
+  uint16_t reading_batch_size_char_handle_ = 0x33;
+  uint16_t measurement_char_handle_ = 0x14;
+
+  uint16_t battery_char_handle_ = 0x10;
+  uint16_t led_sensitivity_char_handle_ = 0x25;
+  uint16_t firmware_char_handle_ = 0x3b;
+  uint16_t uuid_char_handle_ = 0x28;
+  uint16_t serial_number_char_handle_ = 0x2b;
 };
 
-}  // namespace powerpal_ble
-}  // namespace esphome
+}  
+}  
 
-#endif  // USE_ESP32
+#endif
